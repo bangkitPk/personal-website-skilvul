abe744387c1a2612f37366f485b65ca59c8cdb0b
bangkit <bangkitputrakristana@gmail.com>
2023-10-07 15:50:00 +0700

    Modify JavaScript for process changing link colors based on the open page

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Change color of active page link navigation text
-document.querySelectorAll("#navbar a").forEach(
-    link => {
-        if(link.href == window.location.href){ // check if link href equal to current link page
-            link.style.color = "#00A3FF";
+const navLinks = document.querySelectorAll("#navbar a");
+if(window.location.href == "https://bangkitpk-personal-website.vercel.app/"){
+    navLinks[0].style.color = "#00A3FF";
+}else {
+    navLinks.forEach(
+        link => {
+            if(link.href == window.location.href){ // check if link href equal to current link page
+                link.style.color = "#00A3FF";
+            }
         }
-    }
-)
+    )
+}
